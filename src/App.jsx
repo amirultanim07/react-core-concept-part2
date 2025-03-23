c7801a1f72d7e5ceed1258675c53a167a60b16ce
amirultanim07 <amirultanim@gmail.com>
2025-03-23 14:32:15 -0600

    added friends

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 
-import './App.css'
 import Counter from './Counter'
 import Batsman from './Batsman'
+import Users from './Users'
+import Friends from './Friends'
+import './App.css'
+import { Suspense } from 'react'
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+
+const fetchFriends = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+}
 
 function App() {
+  const friendsPromise = fetchFriends();
 
   function handleClick() {
     alert('I am Clicked.')
@@ -21,13 +33,22 @@ function App() {
     <>
 
       <h3>Vite + React</h3>
+
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h3>Friends Are Coming...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+
       <Batsman></Batsman>
       <Counter></Counter>
 
       <button onClick={handleClick}>Click Me</button>
       <button onClick={handleClick2}>Click Me 2</button>
       <button onClick={() => alert('click 3')}>Click Me 3</button>
-      <button onClick={()=>(handleAdd5(10))}>Add 5</button>
+      <button onClick={() => (handleAdd5(10))}>Add 5</button>
     </>
   )
 }
